10fbaae412f99be06f21e70859e0b2224116d27d
liwei <hi@liwei.me>
2015-03-02 16:51:57 +0800

    update user confirmable

--- chapter_3/shop3.3/db/schema.rb
+++ chapter_3/shop3.3/db/schema.rb
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20150224071758) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
