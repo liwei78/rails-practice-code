1c2791e00c539f4cd8787974ab878c680cf7c5ea
liwei <hi@liwei.me>
2015-04-18 02:40:53 +0800

    UPDATE chapter 4 shop code

--- chapter_4/shop/app/assets/javascripts/united.js
+++
@@ -1,6 +0,0 @@
-// United
-// Bootswatch
-//= require jquery
-//= require jquery_ujs
-//= require united/loader
-//= require united/bootswatch
--- chapter_4/shop/app/assets/javascripts/united/bootswatch.js
+++
@@ -1,3 +0,0 @@
-// United
-// Bootswatch
-// bootswatch.js
