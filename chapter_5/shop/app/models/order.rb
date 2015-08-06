1bb4694cb6592a860c8e160ecd14af450a72e80e
liwei <hi@liwei.me>
2015-08-07 00:11:41 +0800

    update 5

--- chapter_5/shop/app/models/order.rb
+++ chapter_5/shop/app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :user, counter_cache: true
-  has_many :line_items
+  has_many :line_items, -> { includes :variant }
   has_many :variants, through: :line_items
   validates :number, presence: true
 
--- chapter_5/shop/spec/models/order_spec.rb
+++ chapter_5/shop/spec/models/order_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   it { should belong_to(:user) }
+  it { should have_many(:line_items) }
 end
